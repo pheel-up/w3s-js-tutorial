abcb1fc2d98d4bc83b587121048dff24d0d382df
pheel-up <pwhitfield1980@gmail.com>
2016-09-09 05:36:33 -0400

    Third Commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # w3s-js-tutorial
 My work on the w3scools.com JavaScript Tutorial
+
+  --Demonstrates skill with:
+    --JS HTML method getElementById()
+    --JS function syntax and external JavaScript
+      file use.
+    --JS data display options:
+      -window.alert()
+      -document.write()
+      -innerHTML
+      -console.log()
+    --JS statement syntax
---
+++ js/display-functions.js
@@ -0,0 +1,22 @@
+function windowAlert() {
+  window.alert(5 + 6);
+}
+
+function docWrite() {
+  document.write(5 + 6);
+}
+
+function innerHtml()  {
+    document.getElementById("demo").innerHTML = 50 + 60;
+}
+
+function consoleLog()  {
+    console.log(5 + 6);
+}
+
+function programFunct()  {
+    var x = 5;
+    var y = 6;
+    var z = x + y;
+    document.getElementById("demo").innerHTML = z;
+}
